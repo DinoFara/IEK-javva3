a33552754f4745b19e3ddb2f8566bd5088b03991
User <User@erg1-pc13>
2022-11-11 15:54:28 +0200

    Prwti allagi tou project.

---
+++ src/main/java/com/iek/if_else/If_else.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.iek.if_else;
+
+/**
+ *
+ * @author User
+ */
+public class If_else {
+
+    public static void main(String[] args) {
+        int x=20;
+        int y=18;
+        
+        if (x>y);
+        
+        System.out.println("x is greater than y");
+        
+        if (y>x);
+        
+        System.out.println("y is greater than x");
+    }
+}
